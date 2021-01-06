deefc57d43da3dbf363db2d737872511265fcd71
Kacper Wdowik <wdowikkacper@gmail.com>
2021-01-06 20:26:40 +0100

    initial commit

---
+++ src/components/CarsList.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+export const CarsList = () => {
+  const [cars, setCars] = useState([]); 
+  useEffect(() => {
+    const fetchCars = async () => {
+      const response = await fetch('http://localhost:4000/cars');
+      setCars(await response.json())
+    }
+    fetchCars();
+  }, [])
+  return (
+    <div>
+      {cars.map((car, index) => <li>[{++index}]{car.name} - {car.price}$</li>)}
+    </div>
+  )
+}
