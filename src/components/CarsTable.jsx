4cb7cb82f5c00a154e5c69dc136fed4ddecbcbe9
Kacper Wdowik <wdowikkacper@gmail.com>
2021-01-07 09:28:54 +0100

    shared presentation

--- src/components/CarsTable.jsx
+++ src/components/CarsTable.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import { useFetchCars } from '../hooks/useFetchCars'
 
-export const CarsTable = () => {
-  const cars = useFetchCars();
+const CarsTablePresentation = ({ cars }) => {
   return (
     <div>
       <table>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-        {cars.map((car, index) => (
-        <tr>
-          <td>{++index}</td>
-          <td>{car.name}</td>
-          <td>{car.price}$</td>
-        </tr>
-        ))}
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cars.map((car, index) => (
+            <tr key={index}>
+              <td>{++index}</td>
+              <td>{car.name}</td>
+              <td>{car.price}$</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   )
 }
+
+const CarsTableContainer = () => {
+  const cars = useFetchCars();
+  return (
+   <CarsTablePresentation cars={cars} />
+  )
+}
+
+export default CarsTableContainer;
